3f993f9fdae256e88ede4d817960c8c353e8b71e
COMPU-S038\daw1 <adrianhoyos098@gmail.com>
2024-09-30 12:44:01 +0200

    nuevo proyecto

---
+++ UD2_Actividad_2/src/com/decroly/daw/Main.java
@@ -0,0 +1,48 @@
+package com.decroly.daw;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//ejercicio 1
+		Scanner reader = new Scanner(System.in);
+	/*	System.out.println("Introduce la edad: ");
+		int edad = reader.nextInt();
+
+		if (edad>= 18)
+		{
+			System.out.println("Eres mayor de edad");
+		}
+			
+		//ejercicio 2
+		
+		reader = new Scanner(System.in);
+		System.out.println("Introduce la edad: ");
+		int edad1 = reader.nextInt();
+
+		if (edad1>= 18)
+		{
+			System.out.println("Eres mayor de edad");
+		}
+		else
+		{
+			System.out.println("Eres menor de edad");
+		}
+			
+		//ejercicio 3
+			
+			for (int i = 1; i <= 20 ; i++) {
+				System.out.println(i);
+			}
+	*/		
+		//ejercicio 4
+			
+		for (int i = 0; i <= 200 ; i+=2) {
+			System.out.println(i);
+		}	
+	}//
+
+}//
