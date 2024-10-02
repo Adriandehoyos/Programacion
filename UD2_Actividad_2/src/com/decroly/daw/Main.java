a36a49f9486cd951af65d8d5e87e3c3b167f247f
COMPU-S038\daw1 <adrianhoyos098@gmail.com>
2024-10-02 09:56:17 +0200

    hasta el ejercicio 7 de la actividad 2

--- UD2_Actividad_2/src/com/decroly/daw/Main.java
+++ UD2_Actividad_2/src/com/decroly/daw/Main.java
@@ -37,12 +37,55 @@ public class Main {
 			for (int i = 1; i <= 20 ; i++) {
 				System.out.println(i);
 			}
-	*/		
+			
 		//ejercicio 4
 			
 		for (int i = 0; i <= 200 ; i+=2) {
 			System.out.println(i);
-		}	
+		}
+			
+		//ejercicio 5
+		
+		for (int i = 1; i <= 200 ; i++) {
+	
+		if(i % 2== 0)	
+		System.out.println("Los numeros pares:"+i);
+		
+		}
+	
+		//ejercicio 6
+		
+		reader = new Scanner(System.in);
+		System.out.println("Introduce el numero maximo: ");
+		int maximo = reader.nextInt();
+
+		for(int i = 0; i <= maximo; i++) {
+			
+			System.out.println(i);
+		}
+		
+		*/
+		
+		//ejericio 7
+		
+		reader = new Scanner(System.in);
+		
+		int nota;
+		
+		System.out.println("Introduce la calificacion del 1 al 10: ");
+		
+		nota = reader.nextInt();
+		
+		switch (nota) {
+		
+		
+		}
+		
+		
+				
+		
+		
+		
 	}//
 
 }//
