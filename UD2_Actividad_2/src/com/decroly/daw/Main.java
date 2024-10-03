649335b755573506800834535086706ef8c8df12
COMPU-S038\daw1 <adrianhoyos098@gmail.com>
2024-10-03 08:54:09 +0200

    Hasta el 9

--- UD2_Actividad_2/src/com/decroly/daw/Main.java
+++ UD2_Actividad_2/src/com/decroly/daw/Main.java
@@ -64,25 +64,79 @@ public class Main {
 			System.out.println(i);
 		}
 		
-		*/
+		
 		
 		//ejericio 7
 		
 		reader = new Scanner(System.in);
 		
-		int nota;
+		double nota;
 		
 		System.out.println("Introduce la calificacion del 1 al 10: ");
 		
-		nota = reader.nextInt();
+		nota = reader.nextDouble();
 		
-		switch (nota) {
+		if (nota < 3) {
 		
-		
+			System.out.println("Muy Deficiente.");
+		}else if (nota < 5) {
+			
+			System.out.println("insuficiente.");
+		}else if (nota < 6) {
+			
+			System.out.println("Suficiente.");
+		}else if (nota < 7) {
+			
+			System.out.println("Bien.");
+		}else if (nota < 9) {
+			
+			System.out.println("Notable.");
+		}else {
+			System.out.println("Sobresaliente.");
 		}
 		
 		
-				
+			
+		//ejercicio 8
+		
+		reader = new Scanner(System.in);
+		
+		int N;
+		int factorial = 1;
+		
+		System.out.println("Introduce un numero positivo: ");
+		N = reader.nextInt();
+		
+		 for (int i = 2; i <= N; ++i) {
+		      factorial *= i;
+		    }
+		
+		if(N<0){
+			
+			System.out.println("Tiene que ser un numero positivo");
+			
+		}else {
+			
+			System.out.println("El factorial de "+ N +" es "+ factorial);
+		}
+		
+		*/
+		//ejercicio 9
+		
+		reader = new Scanner(System.in);
+		
+		int horas;
+		int mins;
+		int segun;
+		
+		System.out.println("Introduce las Horas: ");
+		horas = reader.nextInt();
+		
+		System.out.println("Introduce los minutos: ");
+		mins = reader.nextInt();
+		
+		System.out.println("Introduce los segundos: ");
+		segun = reader.nextInt();
 		
 		
 		
