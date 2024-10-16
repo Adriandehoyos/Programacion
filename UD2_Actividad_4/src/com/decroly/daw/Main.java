9f91843fb7713551c98ec5c4c8c46c9f4a72fd0e
COMPU-S038\daw1 <adrianhoyos098@gmail.com>
2024-10-16 09:57:28 +0200

    a

--- UD2_Actividad_4/src/com/decroly/daw/Main.java
+++ UD2_Actividad_4/src/com/decroly/daw/Main.java
@@ -11,10 +11,36 @@ public class Main {
 		
 		//ejercicio1
 		
-		int nums[]= new int[10];
+		double[] nums = new double[10];
+		int i;
 		
-		nums = reader.nextInt();
+		System.out.println("Introduce 10 numeros reales");
 		
+		for(i = 0; i < nums.length; i++)
+			nums[i] = reader.nextDouble();
+		
+		System.out.println("Los numeros introducidos son: ");
+		
+		for(i = 0; i < nums.length; i++)
+			System.out.println(nums[i]+ "");
+		
+		//ejercicio 2
+		reader = new Scanner(System.in);
+		
+		double[] vector = new double[10];
+		int u;
+		
+		double suma = 0;
+		
+		System.out.println("Introduce 10 numeros reales");
+		
+		for(u = 0; u < vector.length; u++)
+			vector[u] = reader.nextDouble();
+		
+		System.out.println("La suma de todos los valores son: ");
+		
+		for(u = 0; u < vector.length; u++)
+			vector[u] = reader.nextDouble();
 		
 		
 	}//
