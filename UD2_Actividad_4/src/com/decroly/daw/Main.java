9865e875f8a30482a9a31c91ec9c7904b8867cfb
COMPU-S038\daw1 <adrianhoyos098@gmail.com>
2024-10-17 08:55:39 +0200

    jueves

--- UD2_Actividad_4/src/com/decroly/daw/Main.java
+++ UD2_Actividad_4/src/com/decroly/daw/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		Scanner reader = new Scanner(System.in);
-		
+	/*	
 		//ejercicio1
 		
 		double[] nums = new double[10];
@@ -30,18 +30,43 @@ public class Main {
 		double[] vector = new double[10];
 		int u;
 		
-		double suma = 0;
+		double suma = 0.0;
 		
 		System.out.println("Introduce 10 numeros reales");
 		
 		for(u = 0; u < vector.length; u++)
 			vector[u] = reader.nextDouble();
 		
-		System.out.println("La suma de todos los valores son: ");
+		for(u = 0; u < vector.length; u++) {
+			
+			suma += vector[u];
+		}
 		
-		for(u = 0; u < vector.length; u++)
-			vector[u] = reader.nextDouble();
+		System.out.println("La suma de todos los valores son:"+suma);
+*/
+		//ejercicio 3
 		
+		reader = new Scanner(System.in);
+		
+		double[] vector1 = new double[10];
+		int i = 0;
+		
+		double maximo = 0.0;
+		double minimo = 0.0;
+		
+		System.out.println("Introduce 10 numeros reales");
+		
+		for(i = 0; i < vector1.length; i++)
+			vector1[i] = reader.nextDouble();
+		
+		for(i = 0; i < vector1.length; i++) {
+					
+			maximo= Math.max(vector1[i], maximo);
+			minimo= Math.min(vector1[i], minimo);
+		}
+		
+		System.out.println("El numero máximo es: "+ maximo);
+		System.out.println("El numero minimo es: " + minimo);
 		
 	}//
 
