3c2c914531e636f06bb32d3a75543069f3f738f5
COMPU-S038\daw1 <adrianhoyos098@gmail.com>
2024-10-24 08:55:55 +0200

    avance clase

--- UD2_Actividad_5/src/com/decroly/daw/main.java
+++ UD2_Actividad_5/src/com/decroly/daw/main.java
@@ -1,10 +1,43 @@
 package com.decroly.daw;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		Scanner reader = new Scanner(System.in);
+	/*	
+		//ejercicio 1
+		
+		System.out.println("Introduce la cadena de texto: ");
+		String S = reader.nextLine();
+		
+		String[] palabras = S.split(" ");
+		
+		for(String palabra : palabras) {
+			System.out.println(palabra);
+		}
+	*/	
+		//ejercicio 2
+		
+		reader = new Scanner(System.in);
+		
+		
+		System.out.println("Introduce la cadena de texto 1: ");
+		String cadena = reader.nextLine();
+		
+		System.out.println("Introduce la cadena de texto 2: ");
+		String cadena2 = reader.nextLine();
+		
+		if(cadena.equalsIgnoreCase(cadena2)) {
+			System.out.println("La cadena 1 y la 2 son iguales");
+		}else {
+			System.out.println("La cadena 2 y la 2 no son iguales");
+		}
+		
+		
+	}//
 
-}
+}//
